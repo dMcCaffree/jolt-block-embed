24b4b011ec68f1641a7f06860e219bbeb6f7a5d0
Dustin McCaffree <dustinnicholasmccaffree@gmail.com>
2021-01-29 02:40:49 -0600

    Ready for launch v1git add .

--- src/jolt-block.js
+++ src/jolt-block.js
@@ -8,7 +8,7 @@ import io from './lib/socket.io';
 import getInheritedBackgroundColor from "./helpers/getInheritedBackgroundColor";
 
 (function () {
-  const isProduction = false;
+  const isProduction = true;
   const baseUrl = isProduction ? 'https://api.joltblock.com' : 'http://localhost:5000';
   const wsUrl = isProduction ? 'https://api.plaudy.com' : 'http://localhost:5001';
   const iframeBaseUrl = isProduction ? 'https://app.joltblock.com' : 'http://localhost:3000';
